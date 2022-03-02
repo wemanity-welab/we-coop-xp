a1af2c6e6b1c68e7a1a7c3a3257f86fa75d3b72f
Ali BELHACENE <abelhacene@wemanity.com>
2022-03-02 17:37:34 +0100

    add mocked adapter

--- src/infrastructure/repositories.module.ts
+++ src/infrastructure/repositories.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JobAdapter } from './job/job.repository.adapter';
 import { JobEntity } from './job/job.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
-import { JobDomain } from '../domain/job/job.domain';
-import { JobService } from '../domain/job/job.service';
 
 @Module({
   imports: [
