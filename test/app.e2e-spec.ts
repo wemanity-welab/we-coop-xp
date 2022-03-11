359edabad711afa8541ddc0ed973d22d0ab31cde
rapatte <agayoso@wemanity.com>
2022-03-11 16:34:13 +0100

    api tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/modules/app.module';
 
@@ -15,10 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/jobs (GET)', () => {
+    return request(app.getHttpServer()).get('/jobs/7').expect(HttpStatus.OK);
   });
 });
