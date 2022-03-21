b7f6054c5804326fb31ba273e26cf0c36ab4e6b8
rapatte <agayoso@wemanity.com>
2022-03-21 11:26:33 +0100

    fix: merge conflicts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-<<<<<<< HEAD
 import { AppModule } from './modules/app.module';
-=======
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
->>>>>>> swagger
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
