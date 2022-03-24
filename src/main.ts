7a21f8216a9b442245ee924c3d219e4b774076e4
rapatte <agayoso@wemanity.com>
2022-03-24 10:09:45 +0100

    fix: api name

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function weCoopXp() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('JOB API')
-    .setDescription('CRUD JOB')
+    .setTitle('WECOOPXP')
+    .setDescription('API WECOOPXP')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
   await app.listen(8080);
 }
-bootstrap();
+weCoopXp();
