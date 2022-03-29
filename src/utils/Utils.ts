642f3345b3fcae7bd7ccc97f6b164716fdeb3242
Ali BELHACENE <abelhacene@wemanity.com>
2022-03-29 22:07:23 +0200

    feat: refactoring of remove duplicate object

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,10 +1,13 @@
 export default class Utils {
   static removeDuplicateObject(array: any[]): any[] {
-    const arrayOfObject: any[] = Array.from(
-      new Set(array.map((object) => object.id)),
-    ).map((id) => {
+    const idOfEachObject: any[] = array.map((object) => object.id);
+
+    const filteredId: any[] = [...new Set(idOfEachObject)];
+
+    const arrayOfObject: any[] = filteredId.map((id) => {
       return array.find((object) => object.id === id);
     });
+
     return arrayOfObject;
   }
 }
