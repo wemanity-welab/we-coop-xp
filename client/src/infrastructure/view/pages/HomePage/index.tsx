4a5c3c9f507978208c4b61103f403734c1e729e8
rapatte <agayoso@wemanity.com>
2022-04-22 11:46:30 +0200

    deleted test

--- client/src/infrastructure/view/pages/HomePage/index.tsx
+++ client/src/infrastructure/view/pages/HomePage/index.tsx
@@ -1,16 +1,7 @@
-import { missionService } from 'domain/services/MissionService';
-import { httpAxios } from 'infrastructure/instances/httpAxios';
-import { missionRepository } from 'infrastructure/repositories/missionRepository';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export function HomePage() {
-  const repository = missionRepository(httpAxios);
-  const service = missionService(repository);
-  const test = () => {
-    service.getMissions().then(res => console.log(res));
-  };
-  test();
   return (
     <>
       <Helmet>
