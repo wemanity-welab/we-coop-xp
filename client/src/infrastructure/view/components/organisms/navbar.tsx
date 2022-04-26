a225336ea1fa9f9ee43c0a2198b96d7762c871be
ali <abelhacene@wemanity.com>
2022-04-26 16:50:08 +0200

    feature: side menu, add logo and background color

---
+++ client/src/infrastructure/view/components/organisms/navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Logo } from '../atoms/logo-wemanity';
+import './navbar.scss';
+
+const MenuWrapper = styled.section`
+  background-color: rgba(74, 122, 169, 0.3);
+  width: 250px;
+  height: 100vh;
+`;
+
+const Navbar = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+`;
+
+const StyledLogo = styled.section`
+  background-color: #ffffff;
+`;
+
+export const MenuComponent = () => {
+  return (
+    <>
+      <div className="MenuWrapper">
+        <StyledLogo />
+        <div className="Navbar">
+          <Link to="/missions">Missions</Link>
+          <Link to="/cooperateurs">Cooperateurs</Link>
+          <Link to="/paramètres"></Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuComponent;
