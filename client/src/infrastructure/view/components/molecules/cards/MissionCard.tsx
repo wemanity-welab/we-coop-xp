3165264b065ad446fa4cc48dae0a519469f2a0cf
rapatte <agayoso@wemanity.com>
2022-04-26 16:20:00 +0200

    feat: listing missions

--- client/src/infrastructure/view/components/molecules/cards/MissionCard.tsx
+++ client/src/infrastructure/view/components/molecules/cards/MissionCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './missionCard.scss';
+import active from './check.png';
+import inactive from './close.png';
 
 const MissionCard = ({ props }) => {
   const { title, client, isActive } = props;
@@ -10,10 +12,27 @@ const MissionCard = ({ props }) => {
           <div className="card__content">
             <h3 className="card__header">{title}</h3>
             <p className="card__info">{client}</p>
-            <div className={`card__status-${isActive ? 'on' : 'off'}`}>
-              {isActive ? 'Active' : 'Inactive'}
+            <div className={`card__status`}>
+              {isActive ? (
+                <>
+                  <img
+                    className="card__status__logo"
+                    src={active}
+                    alt="check"
+                  />
+                  <span>Active</span>
+                </>
+              ) : (
+                <>
+                  <img
+                    className="card__status__logo"
+                    src={inactive}
+                    alt="check"
+                  />
+                  <span>Inactive</span>
+                </>
+              )}
             </div>
-            <button className="card__button">Détails</button>
           </div>
         </div>
       </div>
