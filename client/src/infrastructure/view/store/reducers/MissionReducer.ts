48a0540f2339c38c5ce7ece6b7fee601c47fef1e
Samira HOUACINE <shouacine@wemanity.com>
2022-04-28 12:12:40 +0200

    feat:  merge store

--- client/src/infrastructure/view/store/reducers/MissionReducer.ts
+++ client/src/infrastructure/view/store/reducers/MissionReducer.ts
@@ -8,10 +8,14 @@ import { State } from '../types/storeTypes';
 export async function missionReducer(state: State<Mission>, action: Action) {
   switch (action.type) {
     case 'display-list-missions': {
+<<<<<<< HEAD
       const repository = missionRepository(httpAxios);
       const missions = await missionService(repository).getMissions();
 
       return { catalog: [...state.catalog, ...missions] };
+=======
+      return { catalog: action.payload };
+>>>>>>> 3ee41b4 (feat:  merge store)
     }
 
     //add mission
