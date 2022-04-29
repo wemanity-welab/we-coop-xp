282a3e959a2931f6c5f7e70b44908b8072cca657
ali <abelhacene@wemanity.com>
2022-04-29 10:01:56 +0200

    feat: side menu

--- client/src/infrastructure/view/components/organisms/navbar.tsx
+++ client/src/infrastructure/view/components/organisms/navbar.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { Logo } from '../atoms/logo-wemanity';
-import './navbar.scss';
-
-const MenuWrapper = styled.section`
-  background-color: rgba(74, 122, 169, 0.3);
-  width: 250px;
-  height: 100vh;
-`;
-
-const Navbar = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-`;
 
 export const MenuComponent = () => {
   return (
     <>
-      <div className="MenuWrapper">
-        <Logo />
-        <div className="Navbar">
-          <Link to="/missions">Missions</Link>
-          <Link to="/cooperateurs">Cooperateurs</Link>
-          <Link to="/paramètres"></Link>
+      <div className="Menu">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <div className="LinkWrapper">
+          <Link className="Link" to="/missions">
+            Missions
+          </Link>
+          <Link className="Link" to="/cooperateurs">
+            Cooperateurs
+          </Link>
+        </div>
+        <div className="MenuFooter">
+          <Link className="Link" to="/paramètres">
+            <img src="/icon-parameter.png" alt="" />
+            &nbsp;Paramètres
+          </Link>
         </div>
       </div>
     </>
