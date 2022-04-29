e3b578022129d9145440ba49b164e98908c7a69a
rapatte <agayoso@wemanity.com>
2022-04-29 10:09:27 +0200

    feat: component menuCard

--- client/src/infrastructure/view/components/molecules/cards/MissionCard.tsx
+++ client/src/infrastructure/view/components/molecules/cards/MissionCard.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { CardMenu } from '../../atoms';
 import active from './check.png';
 import inactive from './close.png';
 
 const MissionCard = ({ props }) => {
   const { title, client, isActive } = props;
+
   return (
     <li>
       <div className="container">
         <div className="card">
           <div className="card__content">
+            <CardMenu status={isActive} />
             <h3 className="card__header">{title}</h3>
             <p className="card__info">{client}</p>
             <div className={`card__status`}>
