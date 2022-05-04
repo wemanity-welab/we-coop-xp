5a44b11440e0cc0aa50f9ef709878125ac812b47
ali <abelhacene@wemanity.com>
2022-05-04 10:03:02 +0200

    update: change convention name files and remove useless import

---
+++ api/src/exposition/Mission/mission.service.adapter.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { MissionDomain } from '../../domain/Mission/mission.domain';
+import { MissionService } from '../../domain/Mission/mission.service';
+import { Mission } from '../../types/Mission';
+
+@Injectable()
+export class MissionServiceAdapter {
+  private missionService: MissionService;
+  constructor(missionService: MissionService) {
+    this.missionService = missionService;
+  }
+
+  public async save(mission: Mission): Promise<MissionDomain> {
+    const missionDomain = new MissionDomain(mission);
+    return this.missionService.save(missionDomain);
+  }
+  public async getAll(): Promise<MissionDomain[]> {
+    return this.missionService.getAll();
+  }
+  public async getOne(missionId: string): Promise<MissionDomain> {
+    return this.missionService.getOne(missionId);
+  }
+  public async remove(missionId: string): Promise<string> {
+    return this.missionService.remove(missionId);
+  }
+  public async update(
+    missionId: string,
+    mission: Partial<MissionDomain>,
+  ): Promise<MissionDomain> {
+    return this.missionService.update(missionId, mission);
+  }
+  public async search(keywords: string[]): Promise<MissionDomain[]> {
+    return this.missionService.search(keywords);
+  }
+}
