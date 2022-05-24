4eca1a80a9cf2be77a0c0c4fd9136bfde80ab44b
ali <abelhacene@wemanity.com>
2022-05-24 17:58:26 +0200

    feat: add localstorage hooks

---
+++ client/src/infrastructure/view/hooks/useLocalStorage.ts
@@ -0,0 +1,26 @@
+// import { useState, useEffect } from 'react';
+
+// function getStorageValue(key, defaultValue) {
+//   const saved = localStorage.getItem(key);
+//   const initial = JSON.parse(saved!);
+//   return initial || defaultValue;
+// }
+
+// export const useLocalStorage = (key, defaultValue) => {
+//   const [value, setValue] = useState(() => {
+//     return getStorageValue(key, defaultValue);
+//   });
+//   useEffect(() => {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   }, [key, value]);
+
+//   return [value, setValue];
+// };
+
+export const setDataInLocalStorage = (key: string, data: object) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+export const getDataInLocalStorage = (key: string) => {
+  return localStorage.getItem(key);
+};
