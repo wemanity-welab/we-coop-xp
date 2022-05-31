d876f2ad211b42b5f0c80f501285e2936fffa288
rapatte <agayoso@wemanity.com>
2022-05-31 14:28:47 +0200

    fix: app name

--- client/src/infrastructure/view/index.tsx
+++ client/src/infrastructure/view/index.tsx
@@ -20,11 +20,11 @@ export function App() {
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - WeLab"
+        defaultTitle="WeLab"
         htmlAttributes={{ lang: i18n.language }}
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta name="description" content="application" />
       </Helmet>
 
       <Switch>
